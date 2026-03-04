79638daac7b5962c0c8a0b9bddc3787e714959b4
SamithaDhananjaya <dhananjayasamitha68@gmail.com>
2026-03-04 10:29:39 +0530

    Create an Axios module

---
+++ dashboardUI/src/api/userApi.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:8080/api/users';
+
+const client = axios.create({
+    API_BASE_URL,
+    timeout: 5000,
+})
+
+
+
+export async function fetchUsers() {
+    try {
+        const response = await client.get(API_BASE_URL);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        throw error;
+    }
+}
